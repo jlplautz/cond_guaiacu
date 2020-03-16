e274bfecc3efe8c69cabfd6516e95d66b58e8b1c
61246880 <jorge.plautz@gmail.com>
2020-03-16 09:56:28 -0300

    Publicado projeto no heroku
    close #6

--- GuaiAcu/settings.py
+++ GuaiAcu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&4e*kj12yeux$i@12tv^ooq*b)tc7phs89%!l7qiat&fkg4ofm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GuaiAcu.wsgi --log-file -
